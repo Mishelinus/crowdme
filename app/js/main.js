96ff1fa0b885ddccec6ed47080b2e88103c6a63d
Mishelinus <mishelin06@yandex.ru>
2021-03-07 22:08:53 +0300

    added html

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,22 @@
 $(function(){
 
- 
+  $('.slider').slick();
   
 
 
 
-  
+ //start tabs 
+
+ $('.presentation__inner .tab').on('click', function(event) {
+   var id = $(this).attr('data-id');
+      $('.presentation__inner').find('.tab-item').removeClass('active-tab').hide();
+      $('.presentation__inner .presentation__tabs').find('.tab').removeClass('active');
+      $(this).addClass('active');
+      $('#'+id).addClass('active-tab').fadeIn();
+      return false;
+   });
+
+ //end tabs 
     
 // Обязательно Должен быть самом конце кода. 
 //иначе всё, что после него не будет работать, 
